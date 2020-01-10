e8315abb900862c3d26a6718192c5e31b8f59aa6
higashi000 <higashi.ict@gmail.com>
2020-01-10 14:48:00 +0900

    Add ws

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ws: null,
+      message: '',
+      sendButton: null,
+    };
+  }
+
+  componentDidMount() {
+    var ws = new WebSocket("ws://localhost:9001");
+
+    ws.onopen = () => {
+      this.setState({ws: ws});
+    }
+
+    ws.onmessage = evt => {
+      console.log(evt.data)
+      const message = JSON.parse(evt.data)
+      this.setState({dataFromServer: message})
+      console.log(message)
+
+      if (message.message === "game start") {
+        this.setState({message: message.card})
+      } else {
+        this.setState({message: message.message})
+      }
+    }
+  }
+
+  sendStr() {
+    this.state.ws.send("{\"request\":\"draw\"}")
+  }
+
+  render() {
+    return (
+      <div>
+      {this.state.message}
+      <button onClick={this.sendStr.bind(this)}></button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('root')
+);
