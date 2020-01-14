cd5e18aae60febc220d7a9084081211320e56e36
higashi000 <higashi.ict@gmail.com>
2020-01-14 17:52:36 +0900

    Add finish screen

--- src/index.js
+++ src/index.js
@@ -8,9 +8,13 @@ class Main extends React.Component {
     this.state = {
       ws: null,
       card: [],
+      rank: '',
+      rankStr: '',
+      ranking: [],
       message: '',
       id: '',
-      sendButton: null,
+      dispButton: false,
+      dispRanking: false,
     };
   }
 
@@ -30,10 +34,31 @@ class Main extends React.Component {
       if (message.message === "game start" || message.message === "draw card") {
         this.setState({card: message.card})
         this.setState({message: message.message})
-      } else if (message.message === "successful connect") {
+        this.setState({dispButton: true})
+      }
+      else if (message.message === "successful connect") {
         this.setState({id: message.id})
         this.setState({message: message.message})
-      } else {
+      }
+      else if (message.message === "game finish") {
+        this.setState({rankStr: "Your Rank: "})
+        this.setState({ranking: message.ranking})
+        this.setState({dispButton: false})
+        this.setState({dispRanking: true})
+
+        console.log(this.state.id)
+        for (var i = 0; i < 4; ++i) {
+          console.log(message.ranking[i])
+          if (message.ranking[i] === this.state.id) {
+            this.setState({rank: String(i + 1)})
+            break
+          }
+        }
+        this.setState({card: []})
+      }
+      else if (message.message === "You win"){
+        this.setState({card: []})
+        this.setState({dispButton: false})
         this.setState({message: message.message})
       }
     }
@@ -48,12 +73,22 @@ class Main extends React.Component {
       <div>
         <div>Your ID :{this.state.id}</div>
         <div>{this.state.message}</div>
-        <div><button onClick={this.sendStr.bind(this)}>Draw</button></div>
+        {this.state.dispButton && (
+          <div><button onClick={this.sendStr.bind(this)}>Draw</button></div>
+        )}
         <div>
           {this.state.card.map((card) => (
             <li>{card}</li>
           ))}
         </div>
+        {this.state.dispRanking && (
+        <div>
+          <li>1st: {this.state.ranking[0]}</li>
+          <li>2nd: {this.state.ranking[1]}</li>
+          <li>3rd: {this.state.ranking[2]}</li>
+          <li>4th: {this.state.ranking[3]}</li>
+        </div>
+        )}
       </div>
     );
   }
